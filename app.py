9935e92c3e02acdf0bbff7e43987f92da578c3dc
Cihan <sahincihan820@gmail.com>
2026-01-10 17:16:22 +0300

    app.py dosyasındaki NameError ve yazım hatası düzeltildi

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True) # debug=True ekledikimport os
+import time
 import redis
-from flask import Flask, jsonify
-import socket
+from flask import Flask
 
+# 1. Önce Flask uygulamasını tanımlıyoruz
 app = Flask(__name__)
 
-# Değişkenleri dış dünyadan (Environment) alıyoruz
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
-
-cache = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+# 2. Redis bağlantısını kuruyoruz (host='redis' olmasına dikkat!)
+cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
-    return cache.incr('hits')
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
-def get_info():
+def hello():
     count = get_hit_count()
-    return jsonify({
-        "mesaj": "Environment Variables Aktif!",
-        "ziyaret_sayisi": count,
-        "baglanilan_redis": REDIS_HOST,
-        "hostname": socket.gethostname()
-    })
+    return f'Selam! Bu sayfa {count} kez görüntülendi.\n'
 
+# 3. EN SONDA uygulamayı çalıştırıyoruz
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
